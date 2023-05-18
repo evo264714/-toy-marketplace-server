d29db849d404f0649ea579161ed6aeb309255ac3
evo264714 <mh264714@gmail.com>
2023-05-18 12:33:01 +0600

    server is runnin successfully and all the required materials have been installed

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env. PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) =>{
+    res.send("Toy market is running")
+})
+
+app.listen(port, () =>{
+    console.log(`Toy marketplace is running on port: ${port}`);
+})
