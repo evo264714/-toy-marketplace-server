a0436080318f5d28e1884c3d414b1f2fe0ff9f96
evo264714 <mh264714@gmail.com>
2023-05-19 14:01:22 +0600

    added json data on mongodb and got the data in the backend

--- index.js
+++ index.js
@@ -27,12 +27,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toyMarket').collection('toys');
+
+    app.get('/toys', async(req,res)=>{
+        const cursor = toysCollection.find()
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
